9ab66d56f14d0c17bebce818a1e82d08a00c10df
Alexander Kosyakov <aekosyakov@gmail.com>
2019-10-08 23:50:54 +0300

    add cell and view base classes

---
+++ Sources/DailyUtils/Cell.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+
+open
+class Cell: UITableViewCell {
+
+    override
+    init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        initialize()
+    }
+
+    required public
+    init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    static
+    var cellID: String {
+        return NSStringFromClass(Self.self)
+    }
+
+    func initialize() { }
+
+}
---
+++ Sources/DailyUtils/View.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+open
+class View: UIView {
+
+    override
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required public
+    init() {
+        super.init(frame: .zero)
+        initialize()
+    }
+
+    required public
+    init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open func initialize() { }
+
+}
