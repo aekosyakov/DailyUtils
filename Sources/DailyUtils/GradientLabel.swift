4962a38c4daaf463f300cb2247178089de78a9fc
Alexander Kosyakov <aekosyakov@gmail.com>
2020-01-27 20:38:20 +0300

    extend uiimageview & uiview extension

---
+++ Sources/DailyUtils/GradientLabel.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+public
+class GradientLabel: UILabel {
+
+    var gradientColors: [CGColor] = []
+
+    override
+    func drawText(in rect: CGRect) {
+        if let gradientColor = drawGradient(colors: gradientColors, in: rect) {
+            self.textColor = gradientColor
+        }
+        super.drawText(in: rect)
+    }
+
+    private
+    func drawGradient(colors: [CGColor], in rect: CGRect) -> UIColor? {
+        let currentContext = UIGraphicsGetCurrentContext()
+        currentContext?.saveGState()
+        defer { currentContext?.restoreGState() }
+
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        guard let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(),
+                                        colors: colors as CFArray,
+                                        locations: nil) else { return nil }
+
+        let context = UIGraphicsGetCurrentContext()
+        context?.drawLinearGradient(gradient,
+                                    start: CGPoint.zero,
+                                    end: CGPoint(x: rect.size.width, y: 0),
+                                    options: [])
+        let gradientImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        guard let image = gradientImage else { return nil }
+        return UIColor(patternImage: image)
+    }
+
+}
